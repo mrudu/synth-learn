2954a505784e6e8b08717bf58a14f3686012e0dd
mrudu <lou.jarak@gmail.com>
2022-08-02 11:29:40 +0200

    Better input of traces - accepts shortened formulaes and then expands traces manually later

--- LTLsynthesis/prefixTreeBuilder.py
+++ LTLsynthesis/prefixTreeBuilder.py
@@ -7,6 +7,30 @@ def sort_trace_function(trace, ordered_inputs):
 	return int(''.join(map(lambda input_proposition: str(
 		ordered_inputs.index(input_proposition)+1),input_filtered_trace)))
 
+def expand_traces(traces, bdd_inputs, ucb):
+	new_traces = []
+	for trace in traces:
+		new_traces.extend(expand_trace(trace, bdd_inputs, ucb))
+	return new_traces
+
+def expand_trace(trace, bdd_inputs, ucb):
+	expanded_traces = []
+	for i in range(len(trace)-2, -1, -2):
+		bdd_i = str_to_bdd(trace[i], ucb)
+		new_traces = []
+		for inp in bdd_inputs:
+			if bdd_i & inp != buddy.bddfalse:
+				str_i = bdd_to_str(inp)
+				if len(expanded_traces) > 0:
+					for t in expanded_traces:
+						new_traces.append([str_i, trace[i+1]] + t)
+				else:
+					new_traces.append([str_i, trace[i+1]])
+		expanded_traces = new_traces
+	return expanded_traces
+
+
+
 def build_prefix_tree(words):
 	root = MealyState('()')
 	root.level = 0
