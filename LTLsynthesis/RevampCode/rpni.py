3ab672fa6622cf48d3a08be6c2a586f09c14c227
Mrudula Balachander <mrudu.balachander@gmail.com>
2023-10-12 07:37:22 +0200

    Fixed fold operation

--- LTLsynthesis/RevampCode/rpni.py
+++ LTLsynthesis/RevampCode/rpni.py
@@ -1,24 +1,39 @@
 from aalpy.automata import MealyState, MealyMachine
 from LTLsynthesis.RevampCode.utils import checkCFSafety, sort_nodes
-import copy
+import copy, logging
+
+logger = logging.getLogger("mergePhaseLogger")
 
 # Merge step of red and blue state
 def merge(mealy_machine: MealyMachine, q_red: MealyState, q_blue: MealyState):
+	logger.debug("Merge {}:{} and {}:{}".format(q_red.index, 
+		q_red.state_id, q_blue.index, q_blue.state_id))
 	for state in mealy_machine.states:  # O(n)
 		for i in state.transitions.keys(): # O(|I|)
 			if state.transitions[i] == q_blue:
 				state.transitions[i] = q_red
+				logger.debug("Adding transition: {} -{}/{}-> {}".format(
+					state.index, i, state.output_fun[i], q_red.index))
 	return fold(mealy_machine, q_red, q_blue)
 
 # Folding red and blue states together (propogating merge)
 def fold(mealy_machine: MealyMachine, q_red: MealyState, q_blue: MealyState): # O(n*(d^2))
+	logger.debug("Fold {} and {}".format(q_red.index, q_blue.index))
 	for i in q_blue.transitions.keys(): # O(|I|)
 		if i in q_red.transitions.keys(): # O(|I|)
 			# Note: Mealy Machine! Merge failed as input/output don't match! 
 			if q_blue.output_fun[i] != q_red.output_fun[i]:
+				logger.debug("Output dont match: {} -{}/{}-> , {}-{}/{}->".format(
+					q_red.index, i, q_red.output_fun[i], 
+					q_blue.index, i, q_blue.output_fun[i]))
 				return False
-			mealy_machine = fold(mealy_machine, q_red.transitions[i], q_blue.transitions[i]) # O(*)
+			if not fold(mealy_machine, q_red.transitions[i], 
+				q_blue.transitions[i]): # O(*)
+				return False
 		else:
+			logger.debug("Adding transition: {} -{}/{}-> {}".format(
+				q_red.index, i, q_blue.output_fun[i],
+				q_blue.transitions[i].index))
 			q_red.transitions[i] = q_blue.transitions[i]
 			q_red.output_fun[i] = q_blue.output_fun[i]
 	return True
@@ -29,9 +44,14 @@ deep copy of mealy machine in case of failed merges.'''
 def rpni_mealy(mealy_machine: MealyMachine, ucb, antichain_vectors, merging_strategy):
 	red = [mealy_machine.initial_state.index] # O(1)
 	blue = set([s.index for s in mealy_machine.initial_state.transitions.values()]) # O(n)
+	logger.debug("red: {}".format(red))
+	logger.debug("blue: {}".format(blue))
 	while len(blue) > 0: # O(n^4)
 		# Storing copy of old machine in case of failed merges
 		old_mealy_machine = copy.deepcopy(mealy_machine) # O(n^2)?
+		logger.debug("________________CURR STATE________________")
+		pretty_print(old_mealy_machine)
+		logger.debug("________________MEALY MACH________________")
 		q_blue = min(blue) # O(n)
 		blue.remove(q_blue) # O(1)
 		canBeMerged = False # O(1)
@@ -44,10 +64,15 @@ def rpni_mealy(mealy_machine: MealyMachine, ucb, antichain_vectors, merging_stra
 			if len(mealy_machine.states[q_red].state_id) > len(
 				mealy_machine.states[q_blue].state_id):
 				continue
+			mergeStatus = merge(mealy_machine, mealy_machine.states[q_red], 
+				mealy_machine.states[q_blue])
+			safetyStatus = checkCFSafety(mealy_machine, ucb, 
+				antichain_vectors)[0]
+			logger.debug("Status of Merge: {}".format(mergeStatus))
+			logger.debug("Status of Safety: {}".format(safetyStatus))
 			# Testing if merge with q_red state is sucessful
-			if merge(mealy_machine, mealy_machine.states[q_red], 
-				mealy_machine.states[q_blue]) and checkCFSafety(
-				mealy_machine, ucb, antichain_vectors)[0]: # O(n)
+			if (mergeStatus and safetyStatus): # O(n)
+				logger.debug("Merge successful. Add new {} state neighbours to blue (resulting from merge)".format(q_red))
 				canBeMerged = True # O(1)
 				# Merge successful. Add new red state neighbours to
 				# blue (resulting from merge)
@@ -55,18 +80,34 @@ def rpni_mealy(mealy_machine: MealyMachine, ucb, antichain_vectors, merging_stra
 					for q in mealy_machine.states[qr].transitions.values(): # O(d)
 						if q.index not in red: # O(n)
 							blue.add(q.index)
+
+				# pretty_print(mealy_machine)
+				logger.debug("red: {}".format(red))
+				logger.debug("blue: {}".format(blue))
 				break
 			# Handling failed merges and restoring unmerged state
 			else:
+				logger.debug("Failed merge: reverting")
 				mealy_machine = copy.deepcopy(old_mealy_machine)
+				# pretty_print(mealy_machine)
+				logger.debug("red: {}".format(red))
+				logger.debug("blue: {}".format(blue))
 		
 		# If cant be merged with any red, make blue as red
 		if not canBeMerged:
 			if q_blue not in red:
 				red.append(q_blue)
 				blue.update([q.index for q in mealy_machine.states[q_blue
-					].transitions.values()])
+					].transitions.values() if q.index not in red])
+			logger.debug("Merge of {} not possible. Making red".format(q_blue))
+			# pretty_print(mealy_machine)
+			logger.debug("red: {}".format(red))
+			logger.debug("blue: {}".format(blue))
+
 	prune_machine(mealy_machine)
+	print("rpni done")
+	print(checkCFSafety(mealy_machine, ucb, antichain_vectors)[0])
+	pretty_print(mealy_machine)
 	return mealy_machine
 
 # Prune Unreachable Nodes
@@ -94,13 +135,14 @@ def pretty_print(mealy_machine):
 	while j < len(mealy_machine.states):
 		state = mealy_machine.states[j]
 		j += 1
-		print(state.state_id)
+		logger.debug("{}:{}".format(state.index, state.state_id))
 		for i in state.transitions.keys():
-			print("{} -({}/{})-> {}".format(state.state_id, i, 
-				state.output_fun[i], state.transitions[i].state_id))
+			logger.debug("{} -({}/{})-> {}".format(state.index, i, 
+				state.output_fun[i], state.transitions[i].index))
 
 # Building the prefix tree automata
 def build_PTA(examples):
+	logger.debug(examples)
 	root = MealyState('e')
 	root.index = 0
 	list_states = [root]
@@ -127,4 +169,19 @@ def build_PTA(examples):
 				list_states.append(new_state)
 				state = new_state
 	mealyTree = MealyMachine(root, list_states)
+
+	# Reindexing based on length of prefixes
+	curr = root
+	count = 0
+	visited_states = []
+	state_queue = [root]
+	while len(state_queue) > 0:
+		curr = state_queue.pop(0)
+		visited_states.append(curr)
+		curr.index = count
+		count += 1
+		for i in curr.transitions.keys():
+			if curr.transitions[i] not in visited_states:
+				state_queue.append(curr.transitions[i])
+	mealyTree.states = visited_states
 	return mealyTree
