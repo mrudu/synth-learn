25a5d18bec1be2ed5a9aa5b178664c0a0491d890
Mrudula Balachander <mrudu.balachander@gmail.com>
2023-10-27 13:38:12 +0200

    prettified synthe

--- synthlearn.py
+++ synthlearn.py
@@ -28,43 +28,51 @@ if args['source'] is not None:
 	with open(args['source'], 'r') as src_file:
 		args = json.load(src_file)
 
-inputs = args['inputs']
-outputs = args['outputs']
-formula = args['formula']
-traces = args['traces']
-file_name = args['destination']
+try:
+	inputs = args['inputs']
+except:
+	while ((inputs is None) or (len(inputs) == 0)):
+		inputs = input("No input propositions entered. \
+			Enter comma-separated input propositions: ")
+try:
+	outputs = args['outputs']
+except:
+	while ((outputs is None) or (len(outputs) == 0)):
+		outputs = input("No output propositions entered. \
+			Enter comma-separated output propositions: ")
 
-print(args)
-
-while ((inputs is None) or (len(inputs) == 0)):
-	inputs = input("No input propositions entered. \
-		Enter comma-separated input propositions: ")
-while ((outputs is None) or (len(outputs) == 0)):
-	outputs = input("No output propositions entered. \
-		Enter comma-separated output propositions: ")
-while ((formula is None) or (len(formula) == 0)):
-	formula = input("No LTL formula entered. Enter LTL formula: ")
+try:
+	formula = args['formula']
+except:
+	while ((formula is None) or (len(formula) == 0)):
+		formula = input("No LTL formula entered. Enter LTL formula: ")
+try:
+	traces = args['traces']
+	traces = list(map(str.strip, traces.split(',')))
+	traces = list(map(lambda x: x.replace('\r', '').replace('#', '.').split('.'), traces))
+	print(traces)
+except:
+	traces = []
+try:
+	k = args['kvalue']
+except:
+	k = 6
+try:
+	file_name = args['destination']
+except:
+	file_name = 'LearnedModel'
 
 inputs = list(map(str.strip, inputs.split(',')))
 outputs = list(map(str.strip, outputs.split(',')))
-if traces is not None:
-	traces = list(map(str.strip, traces.split(',')))
-	traces = list(map(lambda x: x.replace('\r', '').replace('#', '.').split('.'), traces))
-else:
-	traces = []
-
-if file_name is None:
-	file_name = 'LearnedModel'
 
 m, stats = build_mealy(traces, formula, inputs, outputs,
-	config['DEFAULT'], 6, 'prefix')
+	config['DEFAULT'], k, 'prefix')
 if m is None:
-	print(stats)
-	print(m)
+	print("Unable to generate model.")
+	print(stats['message'])
 else:
+	print("REALIZABLE! Model generated.")
 	save_mealy_machile(m, file_name, ['pdf'])
-	print({ 
-		'traces': stats['traces'],
-		'realizable': stats['realizable'],
-		'message': stats['message']
-	})
+	print("Traces used are:")
+	print('\n'.join(map(lambda t: '#'.join([t[i] + '.' + t[i + 1] 
+       for i in range(0, len(t), 2)]), stats['traces'])))
